d77ee8e93b6072d48d07fb19395a72d210027365
Akuli <akuviljanen17@gmail.com>
2017-01-28 10:15:47 +0200

    improving things

--- bananadoc.py
+++ bananadoc.py
@@ -75,18 +75,8 @@ class NotDocumented(Exception):
     raising this directly, but catching NotDocumented may be useful.
     """
 
-    def __init__(self, where, name):
-        # We don't call super().__init__() here because we don't want an
-        # args attribute. This is a documented feature, not an
-        # implementation detail.
-        self.where = where
-        self.name = name
-
     def __str__(self):
-        return self.where + '.' + self.name
-
-    def __repr__(self):
-        return '%s(%r, %r)' % (type(self).__name__, self.where, self.name)
+        return '.'.join(self.args)
 
 
 class NoDocstring(NotDocumented):
@@ -106,13 +96,23 @@ class NoAll(NotDocumented):
 class Writer:
     """An object for writing markdown strings to a file object easily.
 
-    This class also adds the titles.
+    The Writers also add titles. When the writing is done, each function 
+    in the `finalizers` list will be called with the writer as the only 
+    argument.
     """
 
     def __init__(self, module, stream):
         self.module = module
         self.stream = stream
+        self.finalizers = []
         self._titlelevel = 1
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *error):
+        for func in self.finalizers:
+            func(self)
 
     def write(self, *args, end='\n\n', **kwargs):
         r"""Write a message to `self.stream`.
@@ -155,28 +155,24 @@ class Writer:
 _dumpfuncs = []
 
 
-def add_dumpfunc(**kwargs):
-    """Add a function that dumps an object.
+def add_dumpfunc(dumpfunc):
+    """Add a function for dumping an object.
 
     This is supposed to be used as a decorator. For example, like this:
 
     ```py
-    @add_dumpfunc(type=str)
+    @add_dumpfunc
     def dump_string(writer, where, name, value):
+        if not isinstance(value, str):
+            return False    # not processed
         writer.write(
             "There's a %s variable that points to the string %r."
             % (name, the_string))
+        return True     # processed
     ```
 
-    This function takes these keyword-only arguments, and it needs
-    exactly one of them:
-    - *matchfunc:* A function that will be called with *value* as the
-        only argument. It should return True if the function can dump
-        documentation for the value and False if not.
-    - *type:* Generate a *matchfunc* that will use `isinstance` with
-        this type.
-
-    The dumpfunc function should take these positional arguments:
+    The dumping function should return True if it did something, and
+    take these positional arguments:
     - *writer:* A [Writer](#writer) object.
     - *where:* A modulename-like string that points to where the value
         came from. For example, 'some.module' or 'some.module.SomeClass'.
@@ -184,21 +180,8 @@ def add_dumpfunc(**kwargs):
         *where*.
     - *value:* The value that will be documented.
     """
-    if len(kwargs) != 1:
-        raise TypeError("expected one keyword argument")
-    key, value = kwargs.popitem()
-    if key == 'type':
-        matchfunc = lambda obj: isinstance(obj, value)  # noqa
-    elif key == 'matchfunc':
-        matchfunc = value
-    else:
-        raise TypeError("unexpected keyword argument %r" % key)
-
-    def inner(dumpfunc):
-        _dumpfuncs.append((matchfunc, dumpfunc))
-        return dumpfunc
-
-    return inner
+    _dumpfuncs.append(dumpfunc)
+    return dumpfunc
 
 
 def dump_object(writer, where, name, obj):
@@ -215,32 +198,51 @@ def dump_object(writer, where, name, obj):
     """
     # We need to reverse because we want to use newly added dump
     # functions first.
-    for matchfunc, dumpfunc in reversed(_dumpfuncs):
-        if matchfunc(obj):
-            dumpfunc(writer, where, name, obj)
+    for dumpfunc in reversed(_dumpfuncs):
+        if dumpfunc(writer, where, name, obj):
+            # It did it.
             return
     # The data dumpfunc should be able to document anything.
     assert False, "the data dumpfunc should have caught %r" % (obj,)
 
 
-@add_dumpfunc(type=object)
+@add_dumpfunc
 def _dump_data(writer, where, name, value):
     writer.write(name, "=", repr(value))
+    return True
 
 
-@add_dumpfunc(matchfunc=inspect.isfunction)
+@add_dumpfunc
 def _dump_function(writer, where, name, func):
+    if not inspect.isfunction(func):
+        return False
     title = name + str(inspect.signature(func))  # e.g. thing(a, b, c)
     if func.__doc__ is None:
         raise NoDocstring(where, name)
     with writer.section(title):
         writer.write(inspect.cleandoc(func.__doc__))
+    return True
 
 
-@add_dumpfunc(type=type)
+@add_dumpfunc
+def _dump_property(writer, where, name, prop):
+    if not isinstance(prop, property):
+        return False
+    if prop.__doc__ is None:
+        raise NoDocstring(where, name)
+    with writer.section("The %s property" % name):
+        writer.write(inspect.cleandoc(prop.__doc__))
+    return True
+
+
+@add_dumpfunc
 def _dump_class(writer, where, name, cls):
+    if not isinstance(cls, type):
+        # It's not a class.
+        return False
     if enum is not None:
-        assert not issubclass(cls, enum.Enum), "the enum dumper didn't work"
+        assert not isinstance(cls, enum.EnumMeta), \
+               "the enum dumper didn't work: %r" % (cls,)
     if cls.__doc__ is None:
         raise NoDocstring(where, name)
 
@@ -275,10 +277,15 @@ def _dump_class(writer, where, name, cls):
                     continue
             dump_object(writer, where + '.' + name, attribute, value)
 
+    return True
+
 
 if enum is not None:
-    @add_dumpfunc(type=enum.EnumMeta)
+    @add_dumpfunc
     def _dump_enum(writer, where, name, enumclass):
+        if not isinstance(enumclass, enum.EnumMeta):
+            return False
+
         with writer.section("enum %s" % name):
             if enumclass.__doc__ is None:
                 lines = ["%s contains these members:" % name]
@@ -288,6 +295,7 @@ if enum is not None:
                 writer.write('\n'.join(lines))
             else:
                 writer.write(inspect.cleandoc(enumclass.__doc__))
+        return True
 
 
 def _clean_summary(line):
@@ -318,8 +326,6 @@ def dump(modulename, module=None, stream=None):
         raise NoAll(modulename)
     its_a_package = hasattr(module, '__path__')
 
-    writer = Writer(module, stream)
-
     doc = inspect.cleandoc(module.__doc__)
     summary, junk, description = doc.partition('\n')
     title = modulename
@@ -327,21 +333,22 @@ def dump(modulename, module=None, stream=None):
         title += " - "
         title += _clean_summary(summary)
 
-    with writer.section(title):
-        writer.write(description.lstrip('\n'))
-        submods = []
-        for name in module.__all__:
-            if its_a_package:
-                try:
-                    importlib.import_module(modulename + '.' + name)
-                    # It's a submodule.
-                    submods.append(modulename + '.' + name)
-                    continue
-                except ImportError:
-                    # It's a variable, just keep going normally.
-                    pass
-            value = getattr(module, name)
-            dump_object(writer, modulename, name, value)
+    with Writer(module, stream) as writer:
+        with writer.section(title):
+            writer.write(description.lstrip('\n'))
+            submods = []
+            for name in module.__all__:
+                if its_a_package:
+                    try:
+                        importlib.import_module(modulename + '.' + name)
+                        # It's a submodule.
+                        submods.append(modulename + '.' + name)
+                        continue
+                    except ImportError:
+                        # It's a variable, just keep going normally.
+                        pass
+                value = getattr(module, name)
+                dump_object(writer, modulename, name, value)
 
     return submods
 
@@ -398,14 +405,18 @@ def main():
         help="assume yes instead of asking questions")
     args = parser.parse_args()
 
-    # The current working directory does not end up on sys.path when
-    # running from a setuptools entry script.
-    for path in sys.path:
-        if os.path.samefile(path, os.curdir):
-            break
-    else:
-        # no breaks
-        sys.path.append(os.curdir)
+    # The current working directory needs to be the first thing on
+    # sys.path because the documented module and the rc file come from
+    # there.
+    if not os.path.samefile(sys.path[0], os.curdir):
+        sys.path.insert(0, os.curdir)
+
+    # This needs to be a local import because unlike most other things,
+    # the rc file is supposed to have side effects when importing.
+    try:
+        import bananadocrc
+    except ImportError:
+        pass
 
     # We need to check for this here because .stuff would later turn
     # into /stuff.
@@ -445,13 +456,20 @@ def main():
             outfile = os.path.join(args.outdir, *parts) + '.md'
         if not args.quiet:
             print(('%s -> %s' % (modname, outfile)).ljust(70), end='\r')
-        with _mkdir_open(outfile, 'w') as f:
-            submodules = dump(modname, module, f)
-            if args.document_submodules:
-                module_queue.extend(submodules)
-            else:
-                undocumented.extend(submodules)
-        documented.append(modname)
+        try:
+            with _mkdir_open(outfile, 'w') as f:
+                submodules = dump(modname, module, f)
+                if args.document_submodules:
+                    module_queue.extend(submodules)
+                else:
+                    undocumented.extend(submodules)
+            documented.append(modname)
+        except Exception as e:
+            # avoid ending up with the traceback on top of the current 
+            # module name
+            if not args.quiet:
+                print()
+            raise e
 
     if not args.quiet:
         # clear the last line from \r tricks and leave it empty
